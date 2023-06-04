c2c0d5583095a7dacd33ea287156baef0c518e57
Matheus Pereira <matheusgp.mto@outlook.com>
2023-06-04 19:41:00 -0300

    Criando o checkbox personalizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { RodapeComponent } from './components/rodape/rodape.component';
 import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
 import { LoginComponent } from './components/login/login.component';
+import { CheckboxComponent } from './components/checkbox/checkbox.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RodapeComponent,
     CabecalhoComponent,
-    LoginComponent
+    LoginComponent,
+    CheckboxComponent
   ],
   imports: [
     BrowserModule,
