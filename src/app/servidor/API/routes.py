b3bf6f9e08b9110e7bc6df4809dfe3f5511335b4
Matheus Pereira <matheusgp.mto@outlook.com>
2023-06-03 23:30:23 -0300

    Criando a função da API que cadastra o usuário no banco de dados

--- src/app/servidor/API/routes.py
+++ src/app/servidor/API/routes.py
@@ -1,6 +1,22 @@
-from API import app, database
+from API import app, database, bcrypt
+from API.models import Usuario
+from flask import request
+import sys
+import ast
 
 
-@app.route("/")
-def teste():
-    return {"teste": "teste"}
+@app.route("/", methods=["POST"])
+def cadastra_usuario():
+    info_usuario = ast.literal_eval(request.data.decode(sys.getdefaultencoding()))
+    usuario = Usuario(
+        nome=info_usuario.nome,
+        sobrenome=info_usuario.sobrenome,
+        email=info_usuario.email,
+        senha=bcrypt.generate_password_hash(info_usuario.senha)
+    )
+
+    with app.app_context():
+        database.session.add(usuario)
+        database.session.commit()
+
+    return ""
