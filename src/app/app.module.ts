9ebda2e85fd1957bdf573fe4f7087e41270f97b5
Matheus Pereira <matheusgp.mto@outlook.com>
2023-06-04 22:37:18 -0300

    Criando a pasta de pages e botões para as páginas de trocar senha e cadastrar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RodapeComponent } from './components/rodape/rodape.component';
 import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
-import { LoginComponent } from './components/login/login.component';
+import { LoginComponent } from './components/pages/login/login.component';
 import { CheckboxComponent } from './components/checkbox/checkbox.component';
 import { MenssagemErroComponent } from './components/menssagem-erro/menssagem-erro.component';
 import { FormsModule } from '@angular/forms';
-import { ValidaLoginDirective } from './diretivas/formulario-login/valida-login.directive';
 import { ValidaCadastroDirective } from './diretivas/formulario-cadastro/valida-cadastro.directive';
 import { HttpClientModule } from '@angular/common/http';
+import { CadastrarComponent } from './components/pages/cadastrar/cadastrar.component';
 
 @NgModule({
   declarations: [
@@ -21,8 +21,8 @@ import { HttpClientModule } from '@angular/common/http';
     LoginComponent,
     CheckboxComponent,
     MenssagemErroComponent,
-    ValidaLoginDirective,
-    ValidaCadastroDirective
+    ValidaCadastroDirective,
+    CadastrarComponent
   ],
   imports: [
     BrowserModule,
--- src/app/diretivas/formulario-login/valida-login.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ValidaLoginDirective } from './valida-login.directive';
-
-describe('ValidaLoginDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ValidaLoginDirective();
-    expect(directive).toBeTruthy();
-  });
-});
