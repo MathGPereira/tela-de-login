9355c51fc04415e7547fffa66e1f6d30e4cc692f
Matheus Pereira <matheusgp.mto@outlook.com>
2023-06-07 22:28:42 -0300

    Excluindo todos os arquivos de teste do projeto

--- src/app/diretivas/formulario-cadastro/valida-cadastro.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ValidaCadastroDirective } from './valida-cadastro.directive';
-
-describe('ValidaCadastroDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ValidaCadastroDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/app/diretivas/valida-senha/valida-mesma-senha.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ValidaMesmaSenhaDirective } from './valida-mesma-senha.directive';
-
-describe('ValidaMesmaSenhaDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ValidaMesmaSenhaDirective();
-    expect(directive).toBeTruthy();
-  });
-});
